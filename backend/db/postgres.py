77916c1c3bc8088e314d5431e4f69b0ea53d066d
Archangelion <tupyakov.daniil@gmail.com>
2026-06-15 13:30:25 +0300

    UPD: set connections pool for SQLAlchecmy

--- backend/db/postgres.py
+++ backend/db/postgres.py
@@ -15,7 +15,14 @@ PG_PORT = os.getenv("POSTGRES_PORT_EXTERNAL")
 
 DATABASE_URL = f"postgresql+asyncpg://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}"
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=5,
+    pool_pre_ping=True,
+)
 AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
